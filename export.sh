ce6132f9af023f5c57195c5ec0bd82ed542fcfa6
Oliver Mattos <omattos@gmail.com>
2017-09-21 10:53:38 +0100

    convert to dxf

--- export.sh
+++ export.sh
@@ -2,9 +2,12 @@
 
 INPUT=test.svg
 
+FILENAME=1
+
 read template1
-cat $INPUT | sed "s/\\\$template1\\\$/${template1}/"
+cat $INPUT | sed "s/\\\$template1\\\$/${template1}/" > ${FILENAME}.svg
 
-inkscape --export-text-to-path -i=g2990 -j -P test.ps test.svg
+inkscape --export-text-to-path -i=g2990 -j --export-eps ${FILENAME}.eps ${FILENAME}.svg
 
 
+pstoedit -f "dxf: -mm -ctl" ${FILENAME}.eps ${FILENAME}.dxf
