f773dd2c1a615648d578e2d611bbed59269b6970
Hello1024 <omattos@gmail.com>
2017-11-19 00:49:30 +0000

    top level script

---
+++ christmas2017/export.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cnt=0
+
+while true; do
+  cp deepdream.py deepdream-copy.py
+  for i in {1..8}
+  do
+
+    read template
+    # Forgive me for such a horrible hack...
+    sed -i "s/\\\$template$i\\\$/${template}/" "deepdream-copy.py"
+
+  done
+  python deepdream-copy.py
+  for FILENAME in front back
+  do
+    inkscape --export-pdf "$(printf "%05d\n" $i)-$FILENAME.pdf" "${FILENAME}.svg"
+  done
+
+  cnt=$((cnt + 1))
+
+done;
